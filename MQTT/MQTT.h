d974afa3871edce81817b3b1194806a4fd1e0242
Karthik Saligrama <coolspark@gmail.com>
2014-10-06 14:36:25 -0700

    Create a Framework.

---
+++ MQTT/MQTT.h
@@ -0,0 +1,15 @@
+//
+//  MQTT.h
+//  MQTT
+//
+//  Created by Karthik Saligrama on 10/6/14.
+//  Copyright (c) 2014 Karthik Saligrama. All rights reserved.
+//
+
+#ifndef MQTT_MQTT_h
+#define MQTT_MQTT_h
+
+#include <MQTT/MQTTClient.h>
+#include <MQTT/MQTTMessage.h>
+
+#endif
